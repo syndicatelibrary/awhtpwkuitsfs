f6d9b80a2dba5eca94fb999b31398f79784285ba
syndicatelibrary <assetslibry@gmail.com>
2026-05-23 23:05:16 +0000

    brh

--- pageSet.js
+++
@@ -1,6 +0,0 @@
-// ══════════════════════════════════════════════════════
-//  TITLE MANAGER
-// ══════════════════════════════════════════════════════
-function setTitle(broadPage, specificArea) {
-  document.title = specificArea ? `${broadPage} | ${specificArea}` : broadPage;
-}
